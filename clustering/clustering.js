cece9cc65dbf43591563c292012dea45bbe66789
SharmilaW <146375906+SharmilaW@users.noreply.github.com>
2024-01-22 21:36:16 -0500

    Zoom 8

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 8,
     center: {lat: 42.605, lng: -81.4448},
   });
   // Create an array of alphabetical characters used to label the markers.
